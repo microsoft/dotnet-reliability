265984077c94df0327081a10b071f076d02ba244
Bryan Arant <bryanar@microsoft.com>
2016-05-16 16:26:13 -0700

    adding python client

--- src/dumpling-service/dumpling.python-client/swagger_client/apis/__init__.py
+++
@@ -1,4 +0,0 @@
-from __future__ import absolute_import
-
-# import apis into api package
-from .dumpling_service_api import DumplingServiceApi
--- src/dumpling-service/dumpling.python-client/swagger_client/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from __future__ import absolute_import
-
-# import models into model package
-from .azure_blob_info import AzureBlobInfo
