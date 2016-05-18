014ea660f89030ebec50957a9f811d90d717bb72
Bryan P. Arant <bryanar@microsoft.com>
2016-05-18 16:11:31 -0700

    Update TriageDb.cs
    - fixed queries
    - added getpropertiesasjsonasync(...)

--- src/triage.database/TriageDb.cs
+++ src/triage.database/TriageDb.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -65,24 +65,28 @@ namespace triage.database
         private const string BUCKET_DATA_QUERY = @"
 WITH [BucketHits]([BucketId], [HitCount], [StartTime], [EndTime]) AS
 (
-    SELECT [B].[Id] AS [BucketId], COUNT([D].[Id]) AS [HitCount], @p0 AS [StartTime], @p1 AS [EndTime]
+    SELECT [B].[BucketId] AS [BucketId], COUNT([D].[DumpId]) AS [HitCount], @p0 AS [StartTime], @p1 AS [EndTime]
     FROM [Dumps] [D]
     JOIN [Buckets] [B]
         ON [D].[BucketId] = [B].[BucketId]
         AND [D].[DumpTime] >= @p0
         AND [D].[DumpTime] <= @p1
-    GROUP BY [B].[Id]
+    GROUP BY [B].[BucketId]
 )
-SELECT [B].*, [H].[HitCount], [H].[Start], [H].[End]
+SELECT [B].*, [H].[HitCount], [H].[StartTime], [H].[EndTime]
 FROM [Buckets] AS [B]
 JOIN [BucketHits] AS [H]
     ON [B].[BucketId] = [H].[BucketId]
 ";
+
+        private const string BUCKET_DATA = @"SELECT * FROM [Buckets]";
         public static async Task<IEnumerable<BucketData>> GetBucketDataAsync(DateTime start, DateTime end)
         {
             using (var context = new TriageDbContext(s_connStr))
             {
-                return await context.Database.SqlQuery<BucketData>(BUCKET_DATA_QUERY, start, end).ToArrayAsync();
+                var data = context.Database.SqlQuery<BucketData>(BUCKET_DATA, start, end);
+                var returnValue = await data.ToArrayAsync();
+                return returnValue;
             }
         }
 
@@ -93,11 +97,33 @@ WHERE [BucketId] = @p0
     AND [DumpTime] >= @p1
     AND [DumpTime] <= @p2
 ";
-        public static async Task<IEnumerable<Dump>> GetBucketDataDumpsAysnc(BucketData bucketData)
+        public static async Task<IEnumerable<Dump>> GetBucketDataDumpsAsync(BucketData bucketData)
         {
             using (var context = new TriageDbContext(s_connStr))
             {
                 return await context.Dumps.SqlQuery(BUCKET_DATA_DUMPS_QUERY, bucketData.BucketId, bucketData.StartTime, bucketData.EndTime).ToArrayAsync();
+            }
+        }
+
+        private const string PROPERTIES_QUERY = @"
+SELECT * 
+FROM [Properties]
+WHERE [DumpId] = @p0
+";
+
+        public static string GetPropertiesAsJsonAsync(Dump dump)
+        {
+            using (var context = new TriageDbContext(s_connStr))
+            {
+                var data = from property in context.Properties.Where(x => x.DumpId == dump.DumpId)
+                        select new
+                        {
+                            DumpId = property.DumpId,
+                            Name = property.Name,
+                            Value = property.Value
+                        };
+
+                return JsonConvert.SerializeObject(data);
             }
         }
     }
