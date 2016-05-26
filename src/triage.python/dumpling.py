4a7c60981ba8a54092eb2a6f83416210f2a70c95
sschaab <sschaab@microsoft.com>
2016-05-26 15:32:20 -0700

    updating dumpling.py to support wrap, unwrap upload and download

--- src/triage.python/dumpling.py
+++ src/triage.python/dumpling.py
@@ -1,25 +1,57 @@
                                                                                                    # Licensed to the .NET Foundation under one or more agreements.
 # The .NET Foundation licenses this file to you under the MIT license.
 # See the LICENSE file in the project root for more information.
+#!/usr/bin/env python
 
 import argparse
 import os
 import zipfile
 import string
+import requests
+import platform
+import getpass     
+import urllib
+import time
 
-def pack(strCorePath, strZipPath):
-    """creates a zip file containing core dump and all related images"""
+class DumplingService:
+    _dumplingUri = 'http://dotnetrp.azurewebsites.net';
+
+    @staticmethod
+    def SayHelloAs(username):
+        hello_url = DumplingService._dumplingUri + '/dumpling/test/hi/im/%s'%(username)
+        response = requests.get(hello_url)
+
+        return response.content
+
+    @staticmethod
+    def UploadZip(filepath):
+        upload_url = DumplingService._dumplingUri + '/dumpling/store/chunk/%s/%s/0/0'%(getpass.getuser(), platform.dist()[0].lower());
+        
+        print 'Uploading core zip ' + args.zipfile  + ' to ' + upload_url
+        
+        files = {'file': open(filepath, 'rb')}
     
-    try:
-        import zlib
-        compressionType = zipfile.ZIP_DEFLATED
-    except:
-        compressionType = zipfile.ZIP_STORED
+        response = requests.post(upload_url, files = files)
 
-    zip = zipfile.ZipFile(strZipPath, mode='w', compression=compressionType, allowZip64=True)
+        return response.content
 
-    add_to_zip(os.path.abspath(strCorePath), zip)
+def pack(strCorePath, strZipPath, lstAddPaths):
+    """creates a zip file containing core dump and all related images"""
 
+    includedFiles = { }
+    
+    #add the core dump to the files to pack
+    includedFiles[os.path.abspath(strCorePath)] = None
+
+
+    for addPath in lstAddPaths:
+        absPath = os.path.abspath(addPath)
+        if os.path.isdir(absPath):
+            for dirpath, dirnames, filenames in os.walk(absPath):
+                for name in filenames:
+                    includedFiles[os.path.join(dirpath, name)] = None
+        else:
+            includedFiles[absPath] = None
     debuggerLoaded = False
 
     try:
@@ -37,16 +69,29 @@ def pack(strCorePath, strZipPath):
 
         debuggerLoaded = True
     except:
-        print 'Unable to load the core file in lldb.  Loaded modules will not be included'
+        print 'Unable to load the core file in degbugger.  Loaded modules will not be included'
 
     #if the core image was loaded into the debugger iterate through the loaded modules and add them to the zip file
     if debuggerLoaded:
         #iterate through image list and pack the zip file
         for m in target.modules:
-            add_to_zip(m.file.fullpath, zip)
+            includedFiles[m.file.fullpath] = None
             if m.file.basename == 'libcoreclr.so':
-                add_to_zip(os.path.join(m.file.dirname, 'libmscordaccore.so'), zip)
-                add_to_zip(os.path.join(m.file.dirname, 'libsos.so'), zip)
+                includedFiles[os.path.join(m.file.dirname, 'libmscordaccore.so')] = None
+                includedFiles[os.path.join(m.file.dirname, 'libsos.so')] = None
+        
+    try:
+        import zlib
+        compressionType = zipfile.ZIP_DEFLATED
+    except:
+        compressionType = zipfile.ZIP_STORED
+        
+    zip = zipfile.ZipFile(strZipPath, mode='w', compression=compressionType, allowZip64=True)
+
+    for k in includedFiles.keys():
+        if k != os.path.abspath(strZipPath):
+            add_to_zip(k, zip)
+
     zip.close()
 
     print 'core dump related files written to: ' + strZipPath 
@@ -56,16 +101,21 @@ def add_to_zip(strPath, zipFile):
         print 'adding ' + strPath
         zipFile.write(strPath)
 
-def unpack(strZipPath):
+def unpack(strZipPath, unpackdir):
     """unpacks zip restoring all files to their original paths"""
     with open(strZipPath, 'rb') as f:
         zip = zipfile.ZipFile(f)
 
         for path in zip.namelist():
             print 'extracting   /' + os.path.join(os.path.basename(strZipPath).replace('.zip', ''), path)
-            zip.extract(path, os.path.join(os.curdir, os.path.basename(strZipPath).replace('.zip', '')))
+            zip.extract(path, unpackdir)
         zip.close()
     print '\nall files extracted\n'
+
+def download(url, zipPath):
+    print("DOWNLOADING " + zipPath + " FROM " + url)
+
+    urllib.urlretrieve(url, filename = zipPath)
 
 def run_command(strCmd, interpreter):
     strOut = ""
@@ -82,20 +132,47 @@ def run_command(strCmd, interpreter):
     return strOut
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Zips and extracts core dumps and their dependent images.', epilog='If the -c option is specified corezip will archive the core along with all its dependent images into the specified zip file (-z).  Otherwise corezip will restore the specifed the coredump and images from the zip file.')
-    parser.add_argument('-z', 
-                      required=True, 
+    parser = argparse.ArgumentParser(description='dumpling client for managing core files and interacting with the dumpling service')
+    parser.add_argument('command',
+                      choices = [ 'wrap', 'unwrap', 'upload', 'download' ],
+                      help='The dumpling command to be run')
+    parser.add_argument('--zipfile', '-z', 
                       type=str,
-                      help='path to the zipfile to create or unpack')
-    parser.add_argument('-c', 
+                      help='path to the core dump zip file')
+                                                            
+    parser.add_argument('--corefile', '-c', 
                       type=str,
-                      help='path to the coredump to be packaged with images')
-
+                      help='path to the core dump file')
+    parser.add_argument('--unpackdir', '-d', 
+                      type=str,
+                      help='path to unpack the core dump zip file')
+    parser.add_argument('--url', '-u', type=str, help='url of dumpling dump to download and unwrap')
+    parser.add_argument('--addpaths', nargs='*', type=str, help='path to additional files to be included in the packaged coredump')
     args = parser.parse_args()
 
-    if args.c is not None and args.c <> "":
-        pack(args.c, args.z)
-    else:
-        unpack(args.z)
+    if args.command == 'wrap':
+        pack(args.corefile, args.zipfile, args.addpaths)
+    elif args.command == 'upload':
+        if args.corefile != None:
+            pack(args.corefile, args.zipfile, args.addpaths)
+        print DumplingService.UploadZip(os.path.abspath(args.zipfile));
+    elif args.command == 'unwrap':
+        if args.unpackdir == None:
+            args.unpackdir = os.path.join(os.getcwd(), os.path.basename(args.zipfile).replace('.zip', '')) + os.path.sep
+        print 'unpacking core dump zip to: ' + args.unpackdir
+        unpack(args.zipfile, args.unpackdir)
+    elif args.command == 'download':
+        if args.unpackdir == None:
+            args.unpackdir = os.path.join(os.getcwd(), 'dumpling.%.7f'%time.time())
+        if args.zipfile == None:
+            args.zipfile = args.unpackdir + '.zip'
+        download(args.url, args.zipfile)
+        if ~os.path.isdir(args.unpackdir):
+            os.mkdir(args.unpackdir)
+        unpack(args.zipfile, args.unpackdir)
+        os.remove(args.zipfile)
+            
+        
+
 
   
