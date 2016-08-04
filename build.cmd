639b0fba21b1a4efcc7f38e1b168120c7432c7f5
Bryan P. Arant <bryanar@microsoft.com>
2016-08-04 12:44:41 -0700

    Testing the removal of maxcpu
    also removed build in parallel

--- build.cmd
+++ build.cmd
@@ -41,7 +41,7 @@ call :build %*
 goto :AfterBuild
 
 :build
-%_buildprefix% msbuild "%_buildproj%" /p:BuildInParallel=true /nologo /maxcpucount /verbosity:minimal /nodeReuse:false /fileloggerparameters:Verbosity=normal;LogFile="%_buildlog%";Append %* %_buildpostfix%
+%_buildprefix% msbuild "%_buildproj%" /nologo /verbosity:minimal /nodeReuse:false /fileloggerparameters:Verbosity=normal;LogFile="%_buildlog%";Append %* %_buildpostfix%
 set BUILDERRORLEVEL=%ERRORLEVEL%
 goto :eof
 
