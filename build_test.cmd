9d85970520e4a1b5f27cbca39b898e4e4604ec8a
Bryan P. Arant <bryanar@microsoft.com>
2016-08-06 16:34:54 -0700

    Update build_test.cmd

--- build_test.cmd
+++ build_test.cmd
@@ -25,7 +25,12 @@ if not defined VisualStudioVersion (
 :EnvSet
 
 :: The property FilterToTestTFM is temporarily required because of  https://github.com/dotnet/buildtools/commit/e9007c16b1832dbd0ea9669fa578b61900b7f724 
-:: Karthik says he will have this removed by EOD.
+echo "USING THE FOLLOWING PARAMETERS (Secret Variables Pasted in Via VSTS):"
+echo CloudDropAccessToken=%1
+echo CloudResultsAccessToken=%2
+echo BuildCompleteConnection="%3"
+echo EventHubSharedAccessKey=%4
+
 call msbuild test/genstress.proj /verbosity:diagnostic /maxcpucount /p:BuildInParallel=true /p:CloudDropAccessToken=%1 /p:CloudResultsAccessToken=%2 /p:BuildCompleteConnection="%3" /p:EventHubSharedAccessKey=%4 /p:FilterToTestTFM=netcoreapp1.0
 set BUILDERRORLEVEL=%ERRORLEVEL%
 
