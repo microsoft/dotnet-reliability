f76bf908509483d9b024f595766dd7c38b1b2853
Bryan P. Arant <bryanar@microsoft.com>
2016-08-07 19:38:37 -0700

    no more git cloning. build in to the file storage

--- build_stress_run.cmd
+++ build_stress_run.cmd
@@ -1,11 +1,20 @@
 :: Need a build-from commandlet
-:: navigate to S:
+:: navigate to our directory
+
+:: TODO:
+::    -- Error Handling
+::    -- Limit number of builds we keep around. When we reach max: as we create a build, we should remove a build.
+::    -- Change naming schema of directories.
+
 cd /d %CloudFileDrive%
 
-git clone %SYSTEM_DEFAULTWORKINGDIRECTORY% %BUILD_BUILDNUMBER%
+mkdir %BUILD_BUILDNUMBER%
 
 cd %BUILD_BUILDNUMBER%
 
 call .\build.cmd
+
+set GeneratedRootPath=%CloudFileDrive%\%BUILD_BUILDNUMBER%\test\
+set BaseIntermediateOutputPath=%CloudFileDrive%\%BUILD_BUILDNUMBER%
 call .\build_test.cmd
 
