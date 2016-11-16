aea10801cb13e2c013e3b2a9fd31fe817f3929e2
sschaab <sschaab@microsoft.com>
2016-11-15 16:25:12 -0800

    moving selfdestruct logic to stress.execution   this will make self destructing smoke test bucket better as the failure will be in a common library as opposed to each generated stress mix

---
+++ src/stress.execution/SelfDestruct.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stress.execution
+{
+    public class SelfDestructException : Exception
+    {
+        public SelfDestructException() : base("The operation self destructed.") { }
+    }
+
+    public static class SelfDestruct
+    {
+        public static void WithException()
+        {
+            throw new SelfDestructException();
+        }
+    }
+}
