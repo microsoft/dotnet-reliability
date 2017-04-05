60efbf9aac9fd556d74b6a2f72a60d87fdc548d8
sschaab <sschaab@microsoft.com>
2017-04-05 09:00:30 -0700

    commiting previously unsaved files

--- src/stress.run/Program.cs
+++ src/stress.run/Program.cs
@@ -5,15 +5,37 @@ using System.Threading.Tasks;
 using System.Reflection;
 using stress.execution;
 using stress.tests;
+using System.Threading;
 
 namespace stress.run
 {
     public class Program
     {
+        private static int s_workerCount = 8;
+        private static TimeSpan s_duration = TimeSpan.FromMinutes(5);
+        private static int s_seed = new Random().Next();
+
         public static void Main(string[] args)
         {
-            Assembly.Load(new AssemblyName("stress.runtime"));
-            
+
+        }
+
+
+        public static void ExecuteTestMix()
+        {
+            CancellationTokenSource completeSource = new CancellationTokenSource(s_duration);
+
+            var unitTests = new StressTestsAssemblyTestEnumerator().ToArray();
+
+            var testPattern = new RandomTestPattern();
+
+            testPattern.Initialize(s_seed, unitTests);
+
+            DedicatedThreadWorkerStrategy workerStrategy = new DedicatedThreadWorkerStrategy();
+
+            StaticLoadPattern loadPattern = new StaticLoadPattern() { WorkerCount = s_workerCount };
+
+            loadPattern.Execute(testPattern, workerStrategy, completeSource.Token);
         }
     }
 }
---
+++ src/stress.tests/StressTestsAssemblyTestEnumerator.cs
@@ -0,0 +1,19 @@
+﻿using stress.execution;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace stress.tests
+{
+    public class StressTestsAssemblyTestEnumerator : UnitTestEnumerator
+    {
+        protected override IEnumerable<UnitTest> GetTests()
+        {
+            return new XunitTestClassEnumerator(typeof(JaggedArray)).Concat(new XunitTestClassEnumerator(typeof(ArrayTests)));
+        }
+    }
+}
